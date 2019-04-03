5ea1bbae372c1504674556621ecb91424c7fcc4f
wanlei <roy.wan@upltv.com>
2019-04-03 17:04:01 +0800

    11

---
+++ Assets/PolyADSDK/test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using Polymer;
+namespace AssemblyCSharp 
+{
+	public class test  : MonoBehaviour
+	{
+
+		public void Start (){
+		
+			UPSDK.initPolyAdSDK(0);
+		}
+
+		public test ()
+		{
+			
+		}
+	}
+}
+
